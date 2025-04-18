f02a2465dd6151698041270bb24a91e4df5145ff
chdasilva647 <chloedasilva647@gmail.com>
2025-04-18 15:25:04 -0400

    Add files via upload

---
+++ Art/sketches/script.js
@@ -0,0 +1,273 @@
+
+$("#S1").click(function () {
+    $("#S1").toggleClass("large");
+    $("#S2").removeClass("large");
+    $("#S3").removeClass("large");
+    $("#S4").removeClass("large");
+    $("#S5").removeClass("large");
+    $("#S6").removeClass("large");
+    $("#S7").removeClass("large");
+    $("#S8").removeClass("large");
+    $("#S9").removeClass("large");
+    $("#S10").removeClass("large");
+    $("#S11").removeClass("large");
+    $("#S12").removeClass("large");
+    $("#S13").removeClass("large");
+    $("#S14").removeClass("large");
+    $("#S15").removeClass("large");
+
+});
+
+$("#S2").click(function () {
+    $("#S2").toggleClass("large");
+    $("#S1").removeClass("large");
+    $("#S3").removeClass("large");
+    $("#S4").removeClass("large");
+    $("#S5").removeClass("large");
+    $("#S6").removeClass("large");
+    $("#S7").removeClass("large");
+    $("#S8").removeClass("large");
+    $("#S9").removeClass("large");
+    $("#S10").removeClass("large");
+    $("#S11").removeClass("large");
+    $("#S12").removeClass("large");
+    $("#S13").removeClass("large");
+    $("#S14").removeClass("large");
+    $("#S15").removeClass("large");
+});
+
+$("#S3").click(function () {
+    $("#S3").toggleClass("large");
+    $("#S1").removeClass("large");
+    $("#S2").removeClass("large");
+    $("#S4").removeClass("large");
+    $("#S5").removeClass("large");
+    $("#S6").removeClass("large");
+    $("#S7").removeClass("large");
+    $("#S8").removeClass("large");
+    $("#S9").removeClass("large");
+    $("#S10").removeClass("large");
+    $("#S11").removeClass("large");
+    $("#S12").removeClass("large");
+    $("#S13").removeClass("large");
+    $("#S14").removeClass("large");
+    $("#S15").removeClass("large");
+});
+
+$("#S4").click(function () {
+    $("#S4").toggleClass("large");
+    $("#S1").removeClass("large");
+    $("#S2").removeClass("large");
+    $("#S3").removeClass("large");
+    $("#S5").removeClass("large");
+    $("#S6").removeClass("large");
+    $("#S7").removeClass("large");
+    $("#S8").removeClass("large");
+    $("#S9").removeClass("large");
+    $("#S10").removeClass("large");
+    $("#S11").removeClass("large");
+    $("#S12").removeClass("large");
+    $("#S13").removeClass("large");
+    $("#S14").removeClass("large");
+    $("#S15").removeClass("large");
+});
+
+$("#S5").click(function () {
+    $("#S5").toggleClass("large");
+    $("#S1").removeClass("large");
+    $("#S2").removeClass("large");
+    $("#S3").removeClass("large");
+    $("#S4").removeClass("large");
+    $("#S6").removeClass("large");
+    $("#S7").removeClass("large");
+    $("#S8").removeClass("large");
+    $("#S9").removeClass("large");
+    $("#S10").removeClass("large");
+    $("#S11").removeClass("large");
+    $("#S12").removeClass("large");
+    $("#S13").removeClass("large");
+    $("#S14").removeClass("large");
+    $("#S15").removeClass("large");
+});
+
+$("#S6").click(function () {
+    $("#S6").toggleClass("large");
+    $("#S1").removeClass("large");
+    $("#S2").removeClass("large");
+    $("#S3").removeClass("large");
+    $("#S4").removeClass("large");
+    $("#S5").removeClass("large");
+    $("#S7").removeClass("large");
+    $("#S8").removeClass("large");
+    $("#S9").removeClass("large");
+    $("#S10").removeClass("large");
+    $("#S11").removeClass("large");
+    $("#S12").removeClass("large");
+    $("#S13").removeClass("large");
+    $("#S14").removeClass("large");
+    $("#S15").removeClass("large");
+});
+
+$("#S7").click(function () {
+    $("#S7").toggleClass("large");
+    $("#S1").removeClass("large");
+    $("#S2").removeClass("large");
+    $("#S3").removeClass("large");
+    $("#S4").removeClass("large");
+    $("#S5").removeClass("large");
+    $("#S6").removeClass("large");
+    $("#S8").removeClass("large");
+    $("#S9").removeClass("large");
+    $("#S10").removeClass("large");
+    $("#S11").removeClass("large");
+    $("#S12").removeClass("large");
+    $("#S13").removeClass("large");
+    $("#S14").removeClass("large");
+    $("#S15").removeClass("large");
+});
+
+$("#S8").click(function () {
+    $("#S8").toggleClass("large");
+    $("#S1").removeClass("large");
+    $("#S2").removeClass("large");
+    $("#S3").removeClass("large");
+    $("#S4").removeClass("large");
+    $("#S5").removeClass("large");
+    $("#S6").removeClass("large");
+    $("#S7").removeClass("large");
+    $("#S9").removeClass("large");
+    $("#S10").removeClass("large");
+    $("#S11").removeClass("large");
+    $("#S12").removeClass("large");
+    $("#S13").removeClass("large");
+    $("#S14").removeClass("large");
+    $("#S15").removeClass("large");
+});
+
+$("#S9").click(function () {
+    $("#S9").toggleClass("large");
+    $("#S1").removeClass("large");
+    $("#S2").removeClass("large");
+    $("#S3").removeClass("large");
+    $("#S4").removeClass("large");
+    $("#S5").removeClass("large");
+    $("#S6").removeClass("large");
+    $("#S7").removeClass("large");
+    $("#S8").removeClass("large");
+    $("#S10").removeClass("large");
+    $("#S11").removeClass("large");
+    $("#S12").removeClass("large");
+    $("#S13").removeClass("large");
+    $("#S14").removeClass("large");
+    $("#S15").removeClass("large");
+});
+
+$("#S10").click(function () {
+    $("#S10").toggleClass("large");
+    $("#S1").removeClass("large");
+    $("#S2").removeClass("large");
+    $("#S3").removeClass("large");
+    $("#S4").removeClass("large");
+    $("#S5").removeClass("large");
+    $("#S6").removeClass("large");
+    $("#S7").removeClass("large");
+    $("#S8").removeClass("large");
+    $("#S9").removeClass("large");
+    $("#S11").removeClass("large");
+    $("#S12").removeClass("large");
+    $("#S13").removeClass("large");
+    $("#S14").removeClass("large");
+    $("#S15").removeClass("large");
+});
+
+
+$("#S11").click(function () {
+    $("#S11").toggleClass("large");
+    $("#S1").removeClass("large");
+    $("#S2").removeClass("large");
+    $("#S3").removeClass("large");
+    $("#S4").removeClass("large");
+    $("#S5").removeClass("large");
+    $("#S6").removeClass("large");
+    $("#S7").removeClass("large");
+    $("#S8").removeClass("large");
+    $("#S9").removeClass("large");
+    $("#S10").removeClass("large");
+    $("#S12").removeClass("large");
+    $("#S13").removeClass("large");
+    $("#S14").removeClass("large");
+    $("#S15").removeClass("large");
+});
+
+$("#S12").click(function () {
+    $("#S12").toggleClass("large");
+    $("#S1").removeClass("large");
+    $("#S2").removeClass("large");
+    $("#S3").removeClass("large");
+    $("#S4").removeClass("large");
+    $("#S5").removeClass("large");
+    $("#S6").removeClass("large");
+    $("#S7").removeClass("large");
+    $("#S8").removeClass("large");
+    $("#S9").removeClass("large");
+    $("#S10").removeClass("large");
+    $("#S11").removeClass("large");
+    $("#S13").removeClass("large");
+    $("#S14").removeClass("large");
+    $("#S15").removeClass("large");
+});
+
+$("#S13").click(function () {
+    $("#S13").toggleClass("large");
+    $("#S1").removeClass("large");
+    $("#S2").removeClass("large");
+    $("#S3").removeClass("large");
+    $("#S4").removeClass("large");
+    $("#S5").removeClass("large");
+    $("#S6").removeClass("large");
+    $("#S7").removeClass("large");
+    $("#S8").removeClass("large");
+    $("#S9").removeClass("large");
+    $("#S10").removeClass("large");
+    $("#S11").removeClass("large");
+    $("#S12").removeClass("large");
+    $("#S14").removeClass("large");
+    $("#S15").removeClass("large");
+});
+
+
+$("#S14").click(function () {
+    $("#S14").toggleClass("large");
+    $("#S1").removeClass("large");
+    $("#S2").removeClass("large");
+    $("#S3").removeClass("large");
+    $("#S4").removeClass("large");
+    $("#S5").removeClass("large");
+    $("#S6").removeClass("large");
+    $("#S7").removeClass("large");
+    $("#S8").removeClass("large");
+    $("#S9").removeClass("large");
+    $("#S10").removeClass("large");
+    $("#S11").removeClass("large");
+    $("#S12").removeClass("large");
+    $("#S13").removeClass("large");
+    $("#S15").removeClass("large");
+});
+
+$("#S15").click(function () {
+    $("#S15").toggleClass("large");
+    $("#S1").removeClass("large");
+    $("#S2").removeClass("large");
+    $("#S3").removeClass("large");
+    $("#S4").removeClass("large");
+    $("#S5").removeClass("large");
+    $("#S6").removeClass("large");
+    $("#S7").removeClass("large");
+    $("#S8").removeClass("large");
+    $("#S9").removeClass("large");
+    $("#S10").removeClass("large");
+    $("#S11").removeClass("large");
+    $("#S12").removeClass("large");
+    $("#S13").removeClass("large");
+    $("#S14").removeClass("large");
+});
