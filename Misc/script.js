5f27260f9abf54e236c2d699768858fe2bc44b6d
chdasilva647 <chloedasilva647@gmail.com>
2025-04-19 21:06:18 -0400

    Update script.js

--- Misc/script.js
+++ Misc/script.js
@@ -1,39 +1,45 @@
-$("#S1").click(function () {
-    $("#S1").toggleClass("large");
-    $("#S2").removeClass("large");
-    $("#S3").removeClass("large");
-    $("#S4").removeClass("large");
-    $("#S5").removeClass("large");
-});
 
-$("#S2").click(function () {
-    $("#S2").toggleClass("large");
-    $("#S1").removeClass("large");
-    $("#S3").removeClass("large");
-    $("#S4").removeClass("large");
-    $("#S5").removeClass("large");
-});
+$("#leatherName").hover(function () {
+    $("#leatherName").css("color", "#133d8a");
+    $("#leatherName").css("background-color", "#ffc400");
+    $("#leatherName").css("transform", "scale(1.1)");
+    $("#leatherName").css("transform", "rotate(5deg)");
+    $("#leatherName").css("cursor", "pointer");
+},
+    function () {
+        $("#leatherName").css("color", "##faf7f7");
+        $("#leatherName").css("background-color", "#133d8a");
+        $("#leatherName").css("transform", "scale(1)");
+        $("#leatherName").css("transform", "rotate(0deg)");
+        $("#leatherName").css("cursor", "default");
+    })
 
-$("#S3").click(function () {
-    $("#S3").toggleClass("large");
-    $("#S1").removeClass("large");
-    $("#S2").removeClass("large");
-    $("#S4").removeClass("large");
-    $("#S5").removeClass("large");
-});
 
-$("#S4").click(function () {
-    $("#S4").toggleClass("large");
-    $("#S1").removeClass("large");
-    $("#S2").removeClass("large");
-    $("#S3").removeClass("large");
-    $("#S5").removeClass("large");
-});
+$("#acrylicName").hover(function () {
+    $("#acrylicName").css("color", "#133d8a");
+    $("#acrylicName").css("background-color", "#ffc400");
+    $("#acrylicName").css("transform", "scale(1.1)");
+    $("#acrylicName").css("cursor", "pointer");
+},
+    function () {
+        $("#acrylicName").css("color", "##faf7f7");
+        $("#acrylicName").css("background-color", "#133d8a");
+        $("#acrylicName").css("transform", "scale(1)");
+        $("#acrylicName").css("cursor", "default");
+    })
 
-$("#S5").click(function () {
-    $("#S5").toggleClass("large");
-    $("#S1").removeClass("large");
-    $("#S2").removeClass("large");
-    $("#S3").removeClass("large");
-    $("#S4").removeClass("large");
-});
+
+$("#ceramicName").hover(function () {
+    $("#ceramicName").css("color", "#133d8a");
+    $("#ceramicName").css("background-color", "#ffc400");
+    $("#ceramicName").css("transform", "scale(1.1)");
+    $("#ceramicName").css("transform", "rotate(-5deg)");
+    $("#ceramicName").css("cursor", "pointer");
+},
+    function () {
+        $("#ceramicName").css("color", "##faf7f7");
+        $("#ceramicName").css("background-color", "#133d8a");
+        $("#ceramicName").css("transform", "scale(1)");
+        $("#ceramicName").css("transform", "rotate(0deg)");
+        $("#ceramicName").css("cursor", "default");
+    })
