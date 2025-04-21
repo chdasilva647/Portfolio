bebcc80ee7fd0e80318b4f14a3dd3358fc183120
chdasilva647 <chloedasilva647@gmail.com>
2025-04-20 23:10:31 -0400

    Add files via upload

---
+++ GD/script.js
@@ -0,0 +1,58 @@
+
+$("#posterName").hover(function () {
+    $("#posterName").css("color", "#133d8a");
+    $("#posterName").css("background-color", "#ffc400");
+    $("#posterName").css("transform", "scale(1.1)");
+    $("#posterName").css("transform", "rotate(5deg)");
+    $("#posterName").css("cursor", "pointer");
+},
+    function () {
+        $("#posterName").css("color", "##faf7f7");
+        $("#posterName").css("background-color", "#133d8a");
+        $("#posterName").css("transform", "scale(1)");
+        $("#posterName").css("transform", "rotate(0deg)");
+        $("#posterName").css("cursor", "default");
+    })
+
+
+$("#typeName").hover(function () {
+    $("#typeName").css("color", "#133d8a");
+    $("#typeName").css("background-color", "#ffc400");
+    $("#typeName").css("transform", "scale(1.1)");
+    $("#typeName").css("cursor", "pointer");
+},
+    function () {
+        $("#typeName").css("color", "##faf7f7");
+        $("#typeName").css("background-color", "#133d8a");
+        $("#typeName").css("transform", "scale(1)");
+        $("#typeName").css("cursor", "default");
+    })
+
+$("#subwayName").hover(function () {
+    $("#subwayName").css("color", "#133d8a");
+    $("#subwayName").css("background-color", "#ffc400");
+    $("#subwayName").css("transform", "scale(1.1)");
+    $("#subwayName").css("cursor", "pointer");
+},
+    function () {
+        $("#subwayName").css("color", "##faf7f7");
+        $("#subwayName").css("background-color", "#133d8a");
+        $("#subwayName").css("transform", "scale(1)");
+        $("#subwayName").css("cursor", "default");
+    })
+
+$("#zineName").hover(function () {
+    $("#zineName").css("color", "#133d8a");
+    $("#zineName").css("background-color", "#ffc400");
+    $("#zineName").css("transform", "scale(1.1)");
+    $("#zineName").css("transform", "rotate(-5deg)");
+    $("#zineName").css("cursor", "pointer");
+},
+    function () {
+        $("#zineName").css("color", "##faf7f7");
+        $("#zineName").css("background-color", "#133d8a");
+        $("#zineName").css("transform", "scale(1)");
+        $("#zineName").css("transform", "rotate(0deg)");
+        $("#zineName").css("cursor", "default");
+    })
+
