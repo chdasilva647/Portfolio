9811c663bc61785b4240c3ef18a3017a4ac93bb3
chdasilva647 <chloedasilva647@gmail.com>
2025-04-22 09:16:47 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,10 @@
-// learned how to use jQuery and hhow to make this simple animation in my graphic design 2 class 
+// learned how to use jQuery and how to make this simple animation in my graphic design 2 class 
 $("#header").hide();
 $("#mainPage").hide();
+$("#icons").hide();
 
 $("#CD").delay(700).fadeOut(400);
 
 $("#mainPage").delay(1000).fadeIn(700);
 $("#header").delay(1000).fadeIn(700);
-
-
-$("#navStudio").click(function(){
-    $("#non-studio").hide();
-    $("#studio").show();
-});
+$("#icons").delay(1000).fadeIn(700);
